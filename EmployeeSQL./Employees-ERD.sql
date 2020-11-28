d2f6e593491aaacf452deeea0a4e961d35051c09
Loic Tiemani <loictiems@github.com>
2020-11-27 22:46:07 -0500

    updating ERD

--- EmployeeSQL./Employees-ERD.sql
+++ EmployeeSQL./Employees-ERD.sql
@@ -1,6 +1,9 @@
-﻿
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
 CREATE TABLE "Department" (
-    "dept_no" varchar(255)   NOT NULL,
+    "dept_no" varchar(20)   NOT NULL,
     "dept_name" varchar(255)   NOT NULL,
     CONSTRAINT "pk_Department" PRIMARY KEY (
         "dept_no"
@@ -8,28 +11,28 @@ CREATE TABLE "Department" (
 );
 
 CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
+    "emp_no" integer   NOT NULL,
+    "salary" integer   NOT NULL,
     CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
      )
 );
 
 CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "emp_title_id" varchar(255)   NOT NULL,
-    "birth_date" date   NOT NULL,
+    "emp_no" integer   NOT NULL,
+    "emp_title_id" varchar(20)   NOT NULL,
+    "birth_date" varchar(10)   NOT NULL,
     "first_name" varchar(255)   NOT NULL,
     "last_name" varchar(255)   NOT NULL,
-    "sex" varchar(20)   NOT NULL,
-    "hire_date" date   NOT NULL,
+    "sex" char(1)   NOT NULL,
+    "hire_date" varchar(10)   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
 CREATE TABLE "titles" (
-    "title_id" varchar(255)   NOT NULL,
+    "title_id" varchar(20)   NOT NULL,
     "title" varchar(255)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
@@ -37,18 +40,18 @@ CREATE TABLE "titles" (
 );
 
 CREATE TABLE "dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL,
+    "emp_no" integer   NOT NULL,
+    "dept_no" varchar(20)   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "emp_no","dept_no"
      )
 );
 
-CREATE TABLE "dep_manager" (
-    "dept_no" varchar(255)   NOT NULL,
-    "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_dep_manager" PRIMARY KEY (
-        "dept_no"
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar(20)   NOT NULL,
+    "emp_no" integer   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
      )
 );
 
@@ -64,9 +67,9 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "Department" ("dept_no");
 
-ALTER TABLE "dep_manager" ADD CONSTRAINT "fk_dep_manager_dept_no" FOREIGN KEY("dept_no")
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "Department" ("dept_no");
 
-ALTER TABLE "dep_manager" ADD CONSTRAINT "fk_dep_manager_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
