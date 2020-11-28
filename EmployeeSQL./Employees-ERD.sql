251a41f0777f6c89e8af90a3f0e610bc2297a43b
Loic Tiemani <loictiems@github.com>
2020-11-28 16:24:04 -0500

    Update Employees-ERD.sql

--- EmployeeSQL./Employees-ERD.sql
+++ EmployeeSQL./Employees-ERD.sql
@@ -81,54 +81,10 @@ REFERENCES "Department" ("dept_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
-
---Data Analysis
---Once you have a complete database, do the following:
-
-
---List the following details of each employee: employee number, last name, first name, sex, and salary.
-
-SELECT employees.emp_no AS "Employee Number"
-		,employees.last_name AS "Last Name"
-		,employees.first_name AS "First name"
-		,employees.sex AS " Gender"
-		,salaries.salary AS "Salary"
-		
-FROM employees JOIN salaries ON employees.emp_no = salaries.emp_no;
-
-
-
-
---List first name, last name, and hire date for employees who were hired in 1986.
-,to_char(employees.hire_date , 'MM/DD/YYYY') AS "Hire_Date"
-SELECT	employees.last_name AS "Last Name"
-		,employees.first_name AS "First Name"
-		,employees.hire_date AS "Hire Date"
-		
-FROM employees
-
-WHERE employees.hire_date BETWEEN '01/01/1986' AND '12/31/1986';		
-
---List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
-SELECT department.dept_no AS "Department Number"
-		,department.dept_name AS "Department Name"
-		,employees.emp_no AS "Employee Number"
-		,employees.last_name AS "Last Name"
-		,employees.first_name AS "First Name"
-
-FROM department JOIN dept_manager ON "Department Number" = dept_manager.dept_no
-	join employees E on DM.emp_no = E.emp_no;
-
-
-
---List the department of each employee with the following information: employee number, last name, first name, and department name.
---List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
---List all employees in the Sales department, including their employee number, last name, first name, and department name.
---List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
---In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-
-
-
-
-
+-- Query * FROM Each Table Confirming Data
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
